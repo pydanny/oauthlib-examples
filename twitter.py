81c0f3a6787542933553d7686359c0cf10d172d7
Daniel Greenfeld <pydanny@gmail.com>
2012-03-14 11:47:32 -0700

    Nixing flask. Want to make it simpler

--- twitter.py
+++ twitter.py
@@ -1,14 +1,39 @@
 import ConfigParser
+
+from oauthlib.oauth import OAuth1aClient
+from oauthlib import parameters
+from oauthlib import signature
+from oauthlib import utils
+
 config = ConfigParser.ConfigParser()
-config.read('tests.cfg')
+config.read('config.cfg')
 
+client_key = config.get('twitter', 'client_key')
+client_secret = config.get("twitter", 'client_secret')
 
-from flask import Flask
-app = Flask(__name__)
+def main():
+    
+    params = {
+            u'oauth_consumer_key':unicode(client_key),
+            u'oauth_nonce':unicode(utils.generate_nonce()),
+            u'oauth_signature_method':u'HMAC-SHA1',
+            u'oauth_timestamp':unicode(utils.generate_timestamp()),
+            u'oauth_token':unicode(client_secret),
+            u'oauth_version':u"1.0"
+        
+        }
+    ordered_params = parameters.order_oauth_parameters(params)
+    normalized_parameters=signature.normalize_parameters(ordered_params)
+    print normalized_parameters
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+    """
+    base_string = signature.construct_base_string(
+        http_method=u"POST",
+        base_string_uri=u"api.twitter.com",
+        normalized_encoded_request_parameters=normalized_parameters
+    )
+    print base_string
+    """
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    main()
